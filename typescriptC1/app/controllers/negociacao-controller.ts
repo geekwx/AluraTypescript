ac94958d13be29e1f788a0c23fc19ee1caf18318
GeekWx <geekwx@gmail.com>
2022-03-12 12:38:22 -0300

    finalizando o curso 2 de typescript

--- typescriptC1/app/controllers/negociacao-controller.ts
+++ typescriptC1/app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiaDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -10,40 +11,49 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+    private readonly SABADO = 6;
+    private readonly DOMINGO = 0;
+
 
     constructor(){
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = <HTMLInputElement>document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void{
+    public adiciona(): void{
         
-        const negociacao = this.criaNegociacao();
-        this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso');
-        this.limparFormulario();
-
-    }
-
-    criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return  new Negociacao(
-            date,
-            quantidade,
-            valor
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
         );
+        if (!this.ehDiaUtil(negociacao.data)){
+            this.mensagemView.update('Apenas negociações em dias uteis são aceitas');
+            return;
+        }
+        this.negociacoes.adiciona(negociacao);
+        this.limparFormulario();
+        this.atualizaView();
+
+    }
+    private ehDiaUtil(data: Date) : boolean {
+        return data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO;
     }
 
-    limparFormulario(): void {
+
+
+    private limparFormulario(): void {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
         this.inputValor.value = "";
         this.inputData.focus();
     }
+
+    private atualizaView(): void{
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
+    };
+
 }
--- typescriptC1/app/views/view.ts
+++ typescriptC1/app/views/view.ts
@@ -1,19 +1,32 @@
-export class View<T> {
+export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    
-    constructor(seletor: string){
-        this.elemento = document.querySelector(seletor);
+    private escapar = false;
+
+
+    constructor(seletor: string, escapar?: boolean){
+        const elemento = document.querySelector(seletor);
+        if(elemento){
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+        }
+        
+
+        if (escapar){
+            this.escapar = escapar;
+        }
 
     }
 
 
-    template(model: T): string {
-     throw Error('Classe filha precisa implementar o metodo template')
-    }
+    protected abstract template(model: T): string;
 
     update(model: T) : void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if(this.escapar){
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 }
